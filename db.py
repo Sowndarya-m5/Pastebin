1383a5db062666bacfffea98fcd2bb26b91f7053
Sowndarya-m5 <msowndarya1110@gmail.com>
2026-01-28 19:41:41 +0530

    Prepare html deployment

--- db.py
+++ db.py
@@ -10,18 +10,15 @@
 #     )
 
 
-
-
-
 import psycopg2
-import os
+from config import DB_HOST, DB_USER, DB_PASSWORD, DB_NAME
 
 def get_db_connection():
-    conn = psycopg2.connect(
-        host=os.environ.get("localhost"),
-        database=os.environ.get("pastedb"),
-        user=os.environ.get("postgres"),
-        password=os.environ.get("Sownd@05"),
-        port=os.environ.get("DB_PORT", 5432)  # default PostgreSQL port
+    return psycopg2.connect(
+        host="localhost",
+        user="postgres",
+        password="Sownd@05",
+        dbname="pastedb"
     )
-    return conn
+
+
