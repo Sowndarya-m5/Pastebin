6643a0c1740b57a9b2708a20a3676cb3b8d07ff0
Sowndarya-m5 <msowndarya1110@gmail.com>
2026-01-28 17:49:53 +0530

    Prepare html deployment

--- db.py
+++ db.py
@@ -1,10 +1,22 @@
-import mysql.connector
+# import mysql.connector
+# from config import DB_HOST, DB_USER, DB_PASSWORD, DB_NAME
+
+# def get_db_connection():
+#     return mysql.connector.connect(
+#         host="localhost",
+#         user="root",
+#         password="Sownd@05",
+#         database="pastebin"
+#     )
+
+
+import psycopg2
 from config import DB_HOST, DB_USER, DB_PASSWORD, DB_NAME
 
 def get_db_connection():
-    return mysql.connector.connect(
+    return psycopg2.connect(
         host="localhost",
         user="root",
         password="Sownd@05",
-        database="pastebin"
+        dbname="pastedb"
     )
